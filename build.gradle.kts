6e55ad73419b9c4d66674b5a2e58ad5905ec0fbd
FiLKoNd <fil.yt.pass@gmail.com>
2024-11-10 05:14:36 +0300

    + reflection

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ subprojects {
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation(kotlin("reflect"))
         compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     }
 
