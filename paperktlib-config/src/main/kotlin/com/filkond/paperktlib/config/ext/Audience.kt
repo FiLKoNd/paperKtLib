4843ae8b371677043939c1f85e13c0f5e305571c
FiLKoNd <fil.yt.pass@gmail.com>
2025-01-28 15:11:50 +0300

    ой

--- paperktlib-config/src/main/kotlin/com/filkond/paperktlib/config/ext/Audience.kt
+++ paperktlib-config/src/main/kotlin/com/filkond/paperktlib/config/ext/Audience.kt
@@ -1,7 +1,7 @@
 package com.filkond.paperktlib.config.ext
 
-import com.filkond.paperktlib.config.templates.Message
+import com.filkond.paperktlib.config.templates.Sendable
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 
-fun Audience.send(message: Message, vararg resolvers: TagResolver) = message.send(this, *resolvers)
+fun Audience.send(message: Sendable, vararg resolvers: TagResolver) = message.send(this, *resolvers)
