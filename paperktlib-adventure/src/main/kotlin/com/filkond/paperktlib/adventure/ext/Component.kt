890b101d58e81e096fcd2cedc9e302dc529ebc84
FiLKoNd <fil.yt.pass@gmail.com>
2025-03-31 13:13:42 +0300

    + placeholder's support for legacy format

--- paperktlib-adventure/src/main/kotlin/com/filkond/paperktlib/adventure/ext/Component.kt
+++ paperktlib-adventure/src/main/kotlin/com/filkond/paperktlib/adventure/ext/Component.kt
@@ -9,13 +9,17 @@ import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 private val mmSerializer = MiniMessage.miniMessage()
 private val legacySerializer = LegacyComponentSerializer.legacyAmpersand()
 
+
 fun String.deserialize(vararg resolvers: TagResolver, isLegacy: Boolean = false): Component =
-    if (isLegacy) legacySerializer.deserialize(this) else mmSerializer.deserialize(this, *resolvers)
+    if (isLegacy) mmSerializer.deserialize(mmSerializer.serialize(legacySerializer.deserialize(this)), *resolvers) // <-- это пиздец
+    else mmSerializer.deserialize(this, *resolvers)
 
 fun <T : Collection<String>> T.deserialize(vararg resolvers: TagResolver, isLegacy: Boolean = false): List<Component> =
     map { it.deserialize(*resolvers, isLegacy = isLegacy) }
 
-fun Component.serialize(isLegacy: Boolean = false): String = let { if (isLegacy) legacySerializer else mmSerializer }.serialize(this)
+fun Component.serialize(isLegacy: Boolean = false): String =
+    let { if (isLegacy) legacySerializer else mmSerializer }.serialize(this)
+
 fun Collection<Component>.serialize(isLegacy: Boolean = false): Collection<String> = map { it.serialize(isLegacy) }
 
 infix fun String.resolver(value: Any): TagResolver = Placeholder.parsed(this, value.toString())
