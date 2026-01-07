8a937862aaa9c11986b61ceae38f32db76dda54a
FiLKoNd <fil.yt.pass@gmail.com>
2026-01-07 07:10:41 +0300

    feat: ext for event invoke

--- paperktlib-paper/src/main/kotlin/com/filkond/paperktlib/paper/ext/Event.kt
+++ paperktlib-paper/src/main/kotlin/com/filkond/paperktlib/paper/ext/Event.kt
@@ -1,7 +1,11 @@
 package com.filkond.paperktlib.paper.ext
 
+import org.bukkit.Bukkit
 import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
 
 fun Cancellable.cancel() {
     isCancelled = true
 }
+
+fun Event.invoke() = Bukkit.getPluginManager().callEvent(this)
